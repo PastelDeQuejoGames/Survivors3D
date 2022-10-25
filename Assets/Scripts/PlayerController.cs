25a41eaaae6d01b82ccbb5d74eb77009d2c35ae7
Matheus Souza <matheus.s.aguiar10@hotmail.com>
2022-10-25 18:39:09 -0300

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 1;
+    [SerializeField] private Rigidbody _rb;
 
     private void Update()
     {
         var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        transform.Translate(dir * _speed * Time.deltaTime);
+        _rb.velocity = dir * _speed;
     }
 }
