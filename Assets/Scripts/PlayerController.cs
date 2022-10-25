0dbd56cb3c643d54589c32156c0e4366e0acc913
Matheus Souza <matheus.s.aguiar10@hotmail.com>
2022-10-25 18:37:50 -0300

    Player and camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1;
+
+    private void Update()
+    {
+        var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        transform.Translate(dir * _speed * Time.deltaTime);
+    }
+}
